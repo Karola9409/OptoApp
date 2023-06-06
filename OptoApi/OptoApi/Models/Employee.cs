fa084a09aada06a4abdf630c70ead59cf481e1fe
Karola <j.karolina9409@gmail.com>
2023-06-06 20:52:09 +0200

    Added employee

---
+++ OptoApi/OptoApi/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace OptoApi.Models
+{
+    public class Employee
+    {
+        public Employee(int employeeId, string firstName, string lastName, string email, EmployeeRole employeeRole, bool isDeleted)
+        {
+            EmployeeId = employeeId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            EmployeeRole = employeeRole;
+            IsDeleted = isDeleted;
+        }
+
+        public int EmployeeId { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public string Email { get; }
+
+        public EmployeeRole EmployeeRole { get; }
+
+        public bool IsDeleted { get; }
+    }
+}
+
---
+++ OptoApi/OptoApi/Validators/EmployeeValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using OptoApi.Models;
+
+namespace OptoApi.Validators
+{
+    public class EmployeeValidator
+    {
+        public ValidationResult IsValid(Employee employee)
+        {
+            if (string.IsNullOrEmpty(employee.FirstName))
+            {
+                return new ValidationResult(false, "Name can't be empty field.");
+            }
+
+            if(string.IsNullOrEmpty(employee.LastName))
+            {
+                return new ValidationResult(false, "Name can't be empty field.");
+            }
+
+            if (!Enum.IsDefined(typeof(EmployeeRole), employee.EmployeeRole))
+            {
+                return new ValidationResult(false, "Employee role is not found.");
+            }
+
+            if (!IsValidEmail(employee.Email))
+            {
+                return new ValidationResult(false, "Email adress is incorrect.");
+            }
+
+            return new ValidationResult(true, "");
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(email);
+        }
+    }
+}
+
